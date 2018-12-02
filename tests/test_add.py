d555c63a4adb043a7c02a3fe1741be5aa2a9aad4
chihuen <510547394@qq.com>
2018-12-02 14:39:47 +0800

    添加项目文件以及测试文件

---
+++ tests/test_add.py
@@ -0,0 +1,9 @@
+
+from project.add import add
+from unittest import TestCase
+
+class StandAloneTests(TestCase):
+	"""Test the stand-alone module functuons."""
+
+	def test_add(self):
+		self.assertTrue(1, 1)
