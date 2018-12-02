2bdcb633a953c54b2e210a16373482febbb19c6f
chihuen <510547394@qq.com>
2018-12-02 23:59:48 +0800

    修改test case

--- project/add.py
+++ project/add.py
@@ -1,3 +1,5 @@
 def add(a, b):
+	if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
+		raise TypeError("a and b should be number")
     return a + b
 
--- tests/test_add.py
+++ tests/test_add.py
@@ -6,4 +6,9 @@ class StandAloneTests(TestCase):
 	"""Test the stand-alone module functuons."""
 
 	def test_add(self):
-		self.assertTrue(1, 1)
+		self.assertEqual(add(1, 1), 2)
+
+	def test_add_param_is_str(self):
+		self.assertRaises(TypeError, add('1', 2))
+		self.assertRaises(TypeError, add(1, '1'))
+		self.assertRaises(TypeError, add('1', '2'))
